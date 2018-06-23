0d93a6d7c6ca9c17df932603155e2355411c9a11
sudoalgorithm <me@knrmalhotra.com>
2018-06-23 22:54:42 +0400

    Chapter 2 Added, Introduction to Disposable

---
+++ Chapter-2/app/src/main/java/com/knrmalhotra/chapter2/MainActivity.java
@@ -0,0 +1,75 @@
+package com.knrmalhotra.chapter2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mTextView;
+    private ArrayList<String> mArrayList;
+
+    private Disposable mDisposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text_view_main);
+        mArrayList = new ArrayList<>();
+
+        Observable<String> teamObservable = getTeamObservable();
+        Observer<String> teamObserver = getTeamObserver();
+
+        teamObservable.observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(teamObserver);
+    }
+
+    private Observer<String> getTeamObserver(){
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+                mDisposable = d;
+            }
+
+            @Override
+            public void onNext(String s) {
+                mArrayList.add(s);
+                mTextView.setText(mArrayList.toString().replaceAll("[\\[\\](){}]",""));
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, e.toString());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete");
+            }
+        };
+    }
+
+    private Observable<String> getTeamObservable(){
+        return Observable.just("Brazil","Germany","England","France","Chile");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDisposable.dispose();
+    }
+}
