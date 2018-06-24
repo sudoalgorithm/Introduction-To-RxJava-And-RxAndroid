de7bcf3b4b47e4f672cbcf34b44868a42ce5dae7
sudoalgorithm <me@knrmalhotra.com>
2018-06-24 14:57:58 +0400

    Chapter-4 Updated

--- Chapter-4/app/src/main/java/com/knrmalhotra/chapter4/MainActivity.java
+++ Chapter-4/app/src/main/java/com/knrmalhotra/chapter4/MainActivity.java
@@ -10,52 +10,96 @@ import java.util.ArrayList;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private TextView mTextView;
-    private ArrayList<String> mArrayList;
+    private TextView mTextView1, mTextView2;
+    private ArrayList<String> mArrayList1, mArrayList2;
 
-    private Disposable mDisposable;
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.knrmalhotra.chapter4.R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(com.knrmalhotra.chapter4.R.id.text_view_main);
-        mArrayList = new ArrayList<>();
+        mTextView1 = (TextView) findViewById(R.id.text_view_main_one);
+        mTextView2 = (TextView) findViewById(R.id.text_view_main_second);
 
-        Observable<String> teamObservable = getTeamObservable();
-        Observer<String> teamObserver = getTeamObserver();
+        mArrayList1 = new ArrayList<>();
+        mArrayList2 = new ArrayList<>();
 
-        teamObservable.observeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
+        Observable<String> animalObservable = getAnimalObservable();
+
+        DisposableObserver<String> animalDisposableObserver = getAnimalDisposableObserver();
+        DisposableObserver<String> animalDisposableObserverAllCaps = getAnimalDisposableObserverAllCaps();
+
+        mCompositeDisposable.add(animalObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .filter(new Predicate<String>() {
                     @Override
                     public boolean test(String s) throws Exception {
                         return s.toLowerCase().startsWith("b");
                     }
                 })
-                .subscribe(teamObserver);
+                .subscribeWith(animalDisposableObserver)
+        );
+
+        mCompositeDisposable.add(animalObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter(new Predicate<String>() {
+                    @Override
+                    public boolean test(String s) throws Exception {
+                        return s.toLowerCase().startsWith("b");
+                    }
+                })
+                .map(new Function<String, String>() {
+                    public String apply(String s) throws Exception{
+                        return s.toUpperCase();
+                    }
+                })
+                .subscribeWith(animalDisposableObserverAllCaps)
+
+        );
+
+
     }
 
-    private Observer<String> getTeamObserver(){
-        return new Observer<String>() {
+    private DisposableObserver<String> getAnimalDisposableObserver(){
+        return new DisposableObserver<String>() {
             @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe");
-                mDisposable = d;
+            public void onNext(String s) {
+                mArrayList1.add(s);
+                mTextView1.setText(mArrayList1.toString().replaceAll("[\\\\[\\\\](){}]",""));
             }
 
             @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    private DisposableObserver<String> getAnimalDisposableObserverAllCaps(){
+        return new DisposableObserver<String>() {
+            @Override
             public void onNext(String s) {
-                mArrayList.add(s);
-                mTextView.setText(mArrayList.toString().replaceAll("[\\[\\](){}]",""));
+                mArrayList2.add(s);
+                mTextView2.setText(mArrayList2.toString().replaceAll("[\\\\[\\\\](){}]",""));
             }
 
             @Override
@@ -65,18 +109,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onComplete() {
-                Log.d(TAG, "onComplete");
+                Log.d(TAG, "All Data Emitted");
             }
         };
     }
 
-    private Observable<String> getTeamObservable(){
-        return Observable.just("Brazil","Germany","England","France","Chile");
+    private Observable<String> getAnimalObservable(){
+        return Observable.fromArray(
+                "Ant", "Ape",
+                "Bat", "Bee", "Bear", "Butterfly",
+                "Cat", "Crab", "Cod",
+                "Dog", "Dove",
+                "Fox", "Frog"
+        );
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mDisposable.dispose();
+        mCompositeDisposable.clear();
     }
 }
